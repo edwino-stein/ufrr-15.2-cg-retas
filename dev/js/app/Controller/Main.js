3d53231bb51b8e2e45b9cc39f31376b304b616f5
Edwino Stein <edwino.stein@gmail.com>
2016-01-11 17:35:04 -0400

    Organizando melhor o Controller.Main

--- dev/js/app/Controller/Main.js
+++ dev/js/app/Controller/Main.js
@@ -3,73 +3,9 @@ App.define('Controller.Main', {
     grid: 'View.Grid',
     canvas: 'View.Canvas',
 
-    getX1: function(){
-
-        var $input = $('#x1');
-            x = parseInt($input.val());
-
-        if(isNaN(x) || x <= 0){
-            $input.val(0);
-            x = 0;
-        }
-        else if(x >= this.canvas.getViewBoxWidth()){
-            x = this.canvas.getViewBoxWidth()
-            $input.val(x);
-        }
-
-        return x;
-    },
-
-    getX2: function(){
-
-        var $input = $('#x2');
-            x = parseInt($input.val());
-
-        if(isNaN(x) || x <= 0){
-            $input.val(0);
-            x = 0;
-        }
-        else if(x >= this.canvas.getViewBoxWidth()){
-            x = this.canvas.getViewBoxWidth()
-            $input.val(x);
-        }
-
-        return x;
-    },
-
-    getY1: function(){
-
-        var $input = $('#y1');
-            y = parseInt($input.val());
-
-        if(isNaN(y) || y <= 0){
-            $input.val(0);
-            y = 0;
-        }
-        else if(y >= this.canvas.getViewBoxWidth()){
-            y = this.canvas.getViewBoxHeight()
-            $input.val(x);
-        }
-
-        return y;
-    },
-
-    getY2: function(){
-
-        var $input = $('#y2');
-            y = parseInt($input.val());
-
-        if(isNaN(y) || y <= 0){
-            $input.val(0);
-            y = 0;
-        }
-        else if(y >= this.canvas.getViewBoxWidth()){
-            y = this.canvas.getViewBoxHeight()
-            $input.val(x);
-        }
-
-        return y;
-    },
+    algorithm: null,
+    gridColor: 'black',
+    canvasColor: 'gray',
 
     canvasToGridX: function(x){
         return Math.floor((this.grid.pixelsWidth * x)/this.canvas.getViewBoxWidth());
@@ -78,22 +14,51 @@ App.define('Controller.Main', {
     canvasToGridY: function(y){
         return Math.floor((this.grid.pixelsHeight * y)/this.canvas.getViewBoxHeight());
     },
-    
-    change: function(e){
 
-        var x1 = this.getX1(),
-            y1 = this.getY1(),
-            x2 = this.getX2(),
-            y2 = this.getY2();
-
-        this.canvas.setX1(x1);
-        this.canvas.setY1(y1);
-        this.canvas.setX2(x2);
-        this.canvas.setY2(y2);
-
-        this.grid.deactivateAllPixels();
+    render: function(point1, point2){
+        if(this.algorithm === null) return;
+        console.log('render');
     },
 
+    updateLine: function(point1, point2){
+        this.canvas.setPoint1(point1);
+        this.canvas.setPoint2(point2);
+        this.canvas.setColor(this.canvasColor);
+
+        this.render(point1, point2);
+    },
+
+    setColor: function(color){
+
+        switch(color){
+            case 'blue':
+                this.gridColor = 'blue';
+                this.canvasColor = 'yellow';
+            break;
+
+            case 'red':
+                this.gridColor = 'red';
+                this.canvasColor = 'cyan';
+            break;
+
+            case 'green':
+                this.gridColor = 'green';
+                this.canvasColor = 'magenta';
+            break;
+
+            case 'yellow':
+                this.gridColor = 'yellow';
+                this.canvasColor = 'blue';
+            break;
+
+            case 'black':
+            default:
+                this.gridColor = 'black';
+                this.canvasColor = 'gray';
+            break;
+        }
+
+    },
 
     ready: function(){
         this.grid.raster(10);
@@ -101,12 +66,8 @@ App.define('Controller.Main', {
 
     init: function(){
         var me = this;
-
         me.grid = me._appRoot_.get(me.grid);
         me.canvas = me._appRoot_.get(me.canvas);
-
-        $('.point-position').change(function(e){me.change(e);})
-                            .on('input', function(e){me.change(e);});
     }
 
 });
